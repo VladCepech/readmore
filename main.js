740b44d1628024963ff56893112909a3ca8c1072
VladCepech <69203308+VladCepech@users.noreply.github.com>
2020-11-02 22:48:51 +0200

    Update main.js
    почему не воспользоваться forEach и parentNode?

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
-//Поиск всех кнопок с классом btn
-let btns = document.querySelectorAll('.btn');
-//Цикл по всем кнопкам
-for (btn of btns) {
-  //Обработка события клика для каждой кнопки
-  btn.addEventListener('click', function() {
-    //Поиск родителя у текущей кнопки с классом card
-    let card = this.closest('.card')
-    //Поиск Точек у родителя (класс dots)
-    let dots = card.querySelector('.dots');
-    //Поиск Текста у родителя (класс more)
-    let more = card.querySelector('.more');
-    //Если точек нет то скрыть текст и показхать точки
-    if(dots.style.display === 'none') {
+'use strict';
+
+document.querySelectorAll('.btn').forEach(function(btn) {
+    btn.addEventListener('click', function() {
+    let dots = btn.parentNode.querySelector('.dots');
+    let more = btn.parentNode.querySelector('.more');
+    
+
+     if(dots.style.display === 'none') {
       dots.style.display = 'inline';
       more.style.display = 'none';
-      this.textContent = 'Подробнее';
+      btn.textContent = 'Подробнее';
     } else {
       //Иначе показать текст и скрыть точки
       dots.style.display = 'none';
       more.style.display = 'inline';
-      this.textContent = 'Скрыть';
+      btn.textContent = 'Скрыть';
     }
-  });
-}
+    });
+});
